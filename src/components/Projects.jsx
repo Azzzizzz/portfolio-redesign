e75f7fdd846649b07df6d9c8d0f98d794ccc3c4e
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-04 21:35:56 +0530

    3. Projects Section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import projectImg from "../img/project-img.png";
 
 export default function Projects() {
   return (
     <div>
-      <h1>projects</h1>
+      <div className="projects">
+        <div className="page-type green-font">
+          <h1>02. Projects</h1>
+        </div>
+        <div className="project-img">
+          <img src={projectImg} alt="" />
+        </div>
+        <div className="details">
+          <div className="project-type green-font">
+            <h2>Project Name</h2>
+          </div>
+          <div className="hide">
+            <h3>Syed Abdul Aziz</h3>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
+            officia adipisci dolore assumenda laborum excepturi libero eligendi
+            fugit, maxime ut consectetur tempora repellendus. Non veritatis
+            neque qui officia sint, voluptas temporibus odit enim iure quasi
+            eius. Quam minima cum ex!
+          </p>
+        </div>
+        <div className="project-img">
+          <img src={projectImg} alt="" />
+        </div>
+        <div className="details">
+          <div className="project-type green-font">
+            <h2>Project Name</h2>
+          </div>
+          <div className="hide">
+            <h3>Syed Abdul Aziz</h3>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
+            officia adipisci dolore assumenda laborum excepturi libero eligendi
+            fugit, maxime ut consectetur tempora repellendus. Non veritatis
+            neque qui officia sint, voluptas temporibus odit enim iure quasi
+            eius. Quam minima cum ex!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
